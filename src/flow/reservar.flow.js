478424c1f96170a34f7ea4c67bf51229a08aec20
Victor Zuluaga <zrvictor@outlook.com>
2024-05-25 14:13:45 -0500

    Enhance: update reserve flow

--- src/flow/reservar.flow.js
+++ src/flow/reservar.flow.js
@@ -1,14 +1,14 @@
 import { addKeyword } from "@builderbot/bot";
 
-export const flowReservar = addKeyword("reservar", "asesor")
+export const flowReservar = addKeyword("reservar", "asesor", "book")
   .addAnswer([
     "En un momento un agente se comunicará contigo para confirmar tu reserva",
   ])
   .addAction(async (ctx, { provider }) => {
-    const message = `Hola, como estas? \nQuiero reservar una habitación \nMi contacto es: \n ${ctx.from} \n${ctx.name}`;
+    const message = `Hola, Quiero hacer una reserva \nMi contacto es: \n${ctx.from} \n${ctx.name}`;
 
     //numero de telefono con el prefijo de tu pais
-    const number = "573112119856";
+    const number = "573218590287";
 
     await provider.sendText(`${number}@s.whatsapp.net`, message);
     console.log(ctx);
