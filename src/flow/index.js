1f25d147dadf6b4d38659ede3617c3853b144ce3
Victor Zuluaga <zrvictor@outlook.com>
2024-05-25 14:13:24 -0500

    Enhance: update principal flow

--- src/flow/index.js
+++ src/flow/index.js
@@ -3,7 +3,7 @@ import { createFlow } from "@builderbot/bot";
 
 // @flows
 import { welcomeFlow } from "./welcome.flow.js";
-import { flowCasa } from "./casa.flow.js";
 import { flowApartamento } from "./apto.flow.js";
+import { flowCasa } from "./casa.flow.js";
 
-export const flow = createFlow([welcomeFlow, flowCasa, flowApartamento]);
+export const flow = createFlow([welcomeFlow, flowApartamento, flowCasa]);
