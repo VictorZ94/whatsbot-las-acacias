a0bb5b3dc6e3d4dd4bb933e65c4571f20d91e8b4
Victor Zuluaga <zrvictor@outlook.com>
2024-05-25 14:12:41 -0500

    Enhance: Update flow casa

--- src/flow/casa.flow.js
+++ src/flow/casa.flow.js
@@ -12,7 +12,7 @@ export const flowCasa = addKeyword(
     "Casa para 10 personas",
     "\nTarifa mínima $350.000 para 4 personas la noche. Persona adicional $70.000. Tarifa total $700.000",
     "*photos:* https://whatsbot-photos.vercel.app/",
-    "\n*reservar* Si desea reservar favor tener clara la fecha de llegada y salida, número de personas y selecciona el siguiente.",
+    "\n*reservar* Si desea reservar favor tener clara la fecha de llegada y salida y número de personas.",
   ],
   { media: "https://i.imgur.com/AcN1rRy.jpg" },
   null,
